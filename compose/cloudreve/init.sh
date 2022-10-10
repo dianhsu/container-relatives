27fa632f10f1ff6b4b8561205bba48fd383a727b
dianhsu <50162344+dianhsu@users.noreply.github.com>
2022-10-10 11:52:51 +0800

    Create init.sh

---
+++ compose/cloudreve/init.sh
@@ -0,0 +1,6 @@
+mkdir -vp cloudreve/{uploads,avatar} \
+&& touch cloudreve/conf.ini \
+&& touch cloudreve/cloudreve.db \
+&& mkdir -p aria2/config \
+&& mkdir -p data/aria2 \
+&& chmod -R 777 data/aria2
